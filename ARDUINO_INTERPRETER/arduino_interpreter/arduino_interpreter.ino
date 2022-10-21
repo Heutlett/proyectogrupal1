09e45b1f0acb03842b343cc1a99d940f40dd9b9f
heutlett <44324449+Heutlett@users.noreply.github.com>
2022-10-21 11:25:39 -0600

    add: se termina la conversion a ascci en el interprete

--- ARDUINO_INTERPRETER/arduino_interpreter/arduino_interpreter.ino
+++ ARDUINO_INTERPRETER/arduino_interpreter/arduino_interpreter.ino
@@ -6,9 +6,6 @@ int b4 = 6;  //
 int b5 = 7;  //
 int b6 = 8;  //
 int b7 = 9;  //
-int o_byte = B00000000;
-int clk_out = 13;
-int flag = 0;
 
 int val0 = 0;  //
 int val1 = 0;  //
@@ -19,9 +16,15 @@ int val5 = 0;  //
 int val6 = 0;  //
 int val7 = 0;  //
 
-int contador = 0;
-
+int clk_out = 13;
 int val_clk = 0;
+
+int o_byte = B00000000;
+int flag = 0;
+int contador = 0;
+int contador2 = 0;
+
+char cadena[100];
 
 void setup() {
   Serial.begin(115200);
@@ -34,23 +37,12 @@ void setup() {
 
 void loop() {
 
-
-  //Serial.println(digitalRead(clk_out));
-
   if (digitalRead(clk_out) == 0) {
     flag = 0;
   }
   if (digitalRead(clk_out) == 1 && flag == 0) {
     flag = 1;
 
-    // val0 = digitalRead(b0);   //
-    // val1 = digitalRead(b1);   //
-    // val2 = digitalRead(b2);   //
-    // val3 = digitalRead(b3);   //
-    // val4 = digitalRead(b4);   //
-    // val5 = digitalRead(b5);   //
-    // val6 = digitalRead(b6);   //
-    // val7 = digitalRead(b7);   //
     bitWrite(o_byte, 0, digitalRead(b0));
     bitWrite(o_byte, 1, digitalRead(b1));
     bitWrite(o_byte, 2, digitalRead(b2));
@@ -60,15 +52,28 @@ void loop() {
     bitWrite(o_byte, 6, digitalRead(b6));
     bitWrite(o_byte, 7, digitalRead(b7));
 
-    if (o_byte != 255) {
+    if (o_byte != 255 & contador != 100) {
+      if (contador == 0) {
+        Serial.println("Se inicia la comunicacion, recepcion de datos:\n");
+      }
+      o_byte = o_byte + 60;  // Quitar esto cuando alejandro logre meter el lorem
+      cadena[contador] = char(o_byte);
       contador = contador + 1;
       Serial.print("Byte(");
       Serial.print(contador);
       Serial.print("): \t");
-      Serial.println(o_byte);
+      Serial.print(o_byte);
+      Serial.print("\t");
+      Serial.println(char(o_byte));
     }
+  }
 
-    
+  if (contador == 100 && contador2 ==0)
+    Serial.println("\n\nCadena resultante del desencriptado:\n");
 
+  if (contador == 100 & contador2 != 100) {
+
+    Serial.print(cadena[contador2]);
+    contador2 = contador2 + 1;
   }
 }
