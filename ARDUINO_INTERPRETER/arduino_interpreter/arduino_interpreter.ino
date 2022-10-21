c105bfbe7944d701bed43204414337e3097b4d8f
heutlett <44324449+Heutlett@users.noreply.github.com>
2022-10-21 10:47:19 -0600

    add: se modifica el interprete y se arregla el bug que solo leia 24 bytes

--- ARDUINO_INTERPRETER/arduino_interpreter/arduino_interpreter.ino
+++ ARDUINO_INTERPRETER/arduino_interpreter/arduino_interpreter.ino
@@ -1,30 +1,30 @@
-int b0 = 2;       // 
-int b1 = 3;       // 
-int b2 = 4;       // 
-int b3 = 5;       //  
-int b4 = 6;       //  
-int b5 = 7;       //  
-int b6 = 8;       //  
-int b7 = 9;       //  
+int b0 = 2;  //
+int b1 = 3;  //
+int b2 = 4;  //
+int b3 = 5;  //
+int b4 = 6;  //
+int b5 = 7;  //
+int b6 = 8;  //
+int b7 = 9;  //
 int o_byte = B00000000;
 int clk_out = 13;
 int flag = 0;
 
-int val0 = 0;     // 
-int val1 = 0;     // 
-int val2 = 0;     //
-int val3 = 0;     // 
-int val4 = 0;     // 
-int val5 = 0;     //
-int val6 = 0;     // 
-int val7 = 0;     // 
+int val0 = 0;  //
+int val1 = 0;  //
+int val2 = 0;  //
+int val3 = 0;  //
+int val4 = 0;  //
+int val5 = 0;  //
+int val6 = 0;  //
+int val7 = 0;  //
 
 int contador = 0;
 
 int val_clk = 0;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   pinMode(b0, INPUT);
   pinMode(b1, INPUT);
   pinMode(b2, INPUT);
@@ -37,21 +37,20 @@ void loop() {
 
   //Serial.println(digitalRead(clk_out));
 
-  if (digitalRead(clk_out)==0){
-    flag=0;
+  if (digitalRead(clk_out) == 0) {
+    flag = 0;
   }
-  if (digitalRead(clk_out)==1 && flag==0){
-    flag=1;
-    contador = contador + 1;
-    Serial.println(contador);
-    // val0 = digitalRead(b0);   // 
-    // val1 = digitalRead(b1);   // 
+  if (digitalRead(clk_out) == 1 && flag == 0) {
+    flag = 1;
+
+    // val0 = digitalRead(b0);   //
+    // val1 = digitalRead(b1);   //
     // val2 = digitalRead(b2);   //
     // val3 = digitalRead(b3);   //
-    // val4 = digitalRead(b4);   // 
+    // val4 = digitalRead(b4);   //
     // val5 = digitalRead(b5);   //
     // val6 = digitalRead(b6);   //
-    // val7 = digitalRead(b7);   // 
+    // val7 = digitalRead(b7);   //
     bitWrite(o_byte, 0, digitalRead(b0));
     bitWrite(o_byte, 1, digitalRead(b1));
     bitWrite(o_byte, 2, digitalRead(b2));
@@ -61,16 +60,15 @@ void loop() {
     bitWrite(o_byte, 6, digitalRead(b6));
     bitWrite(o_byte, 7, digitalRead(b7));
 
-    // Serial.print(val7);
-    // Serial.print(val6);
-    // Serial.print(val5);
-    // Serial.print(val4);
-    // Serial.print(val3);
-    // Serial.print(val2);
-    // Serial.print(val1);
-    //Serial.println(val0);
-    Serial.print("Byte: ");
-    Serial.println(o_byte);
-  }
+    if (o_byte != 255) {
+      contador = contador + 1;
+      Serial.print("Byte(");
+      Serial.print(contador);
+      Serial.print("): \t");
+      Serial.println(o_byte);
+    }
 
-} 
+    
+
+  }
+}
